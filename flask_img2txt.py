8f0f19a2773ffb468ebd5ff682c5b1e35f7a43da
Samyak S Sarnayak <samyak201@gmail.com>
2019-06-02 19:31:12 +0530

    Added comments

--- flask_img2txt.py
+++ flask_img2txt.py
@@ -5,49 +5,53 @@ from werkzeug import secure_filename # pylint: disable=import-error
 from img2textSamarth import ocr_core
 import os
 import sys
-# import nltk
+from collections import OrderedDict
 import string
 from google_images_scraper import runSpider
-app = Flask(__name__)
-UPLOAD_FOLDER = 'static/uploads/'
+
+app = Flask(__name__) #initialize flask object
+UPLOAD_FOLDER = 'static/uploads/' #folder where uploaded images are to be stored
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
-# words=[]
-# urls=[]
-words = dict()
-filename=""
-text=""
-@app.route("/", methods=["GET", "POST"])
+words = OrderedDict() #ordered dictionary to store words and corresponding image url
+filename="" #to store name of uploaded image file
+text="" #to store text extracted from image
+@app.route("/", methods=["GET", "POST"]) #first argument is url where the page will be (relative to localhost:5000)
 def index():
     global filename
     global text
-    global words
+    global words #use global variables
+    #if image is uploaded
     if request.method == "POST":
-        if "file" not in request.files:
-            return render_template("index.html", msg="No file selected")
-        file = request.files["file"]
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        text = ocr_core(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        text = text.replace('\n', '<br>')
-        words = {word:"" for word in [word.strip(string.punctuation) for word in text.replace("<br>", " ").split()]}
+        if "file" not in request.files: #if file is not uploaded
+            return render_template("index.html", msg="No file selected") #Display message, {{msg}} in the html is replaced with the message
+        file = request.files["file"] #get uploaded file
+        filename = secure_filename(file.filename) #get file name
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename)) #save file to upload folder
+        text = ocr_core(os.path.join(app.config['UPLOAD_FOLDER'], filename)) #get image text using ocr_core function from img2txt
+        text = text.replace('\n', '<br>').lower() #replace newline with <br> so that it is rendered properly in the html, also converts to lowecase
+        words = OrderedDict((word,"") for word in [word.strip(string.punctuation) for word in text.replace("<br>", " ").split()]) #Get list of words from the text
         print(words, file=sys.stderr)
+        #load index.html again with the appropriate message, image source, words list
         return render_template("index.html", msg="File uploaded successfully", extracted_text=text, img_src=os.path.join(app.config['UPLOAD_FOLDER'], filename), words=words.items())#{word:url for (word, url) in zip(words, urls)})
+    #if any "Search for image button is clicked"
     elif request.method == "GET":
-        word = request.args.get("word")
-        if word is not None:
-            runSpider(word)
-            words[word] = os.path.join(app.config['UPLOAD_FOLDER'], word + " 0.jpg")
+        word = request.args.get("word") #get word
+        if word is not None: #if word is found
+            runSpider(word) #runs google image scraper (from google_images_scraper.py) to get download the image
+            words[word] = os.path.join(app.config['UPLOAD_FOLDER'], word + " 0.jpg") #add image url to the dict
             print(words[word], file=sys.stderr)
+        #load index.html again with the appropriate message, image source, words list
         return render_template("index.html", words=words.items(), msg="File uploaded successfully", extracted_text=text, img_src=os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    #when the page is first loaded
     else:
         return render_template("index.html")
 
+#to show image
 @app.route("/static/uploads/<filename>")
 def uploaded_file(filename):
-    return send_from_directory(UPLOAD_FOLDER, filename)
+    return send_from_directory(UPLOAD_FOLDER, filename) #send image
 
-# app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+#run flask app when script is run directly
 if __name__ == "__main__":
     app.run(debug=True)
-    # app.run()
--- google_images_scraper.py
+++ google_images_scraper.py
@@ -4,61 +4,33 @@ import csv
 from scrapy.crawler import CrawlerProcess, CrawlerRunner
 import os
 
+#spider class (extends scrapy.Spider)
 class ImageSpider(scrapy.Spider):
-    name = "image_spider"
-    # wordlist = []
-    # with open("wordlist.csv", "r") as csvfile:
-    #     readCSV = csv.reader(csvfile)
-    #     for row in readCSV:
-    #         wordlist += row
+    name = "image_spider" #name of spider
 
-
+    #to set user agent so that images can be downloaded without being blocked
     opener=urllib.request.build_opener()
     opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
     urllib.request.install_opener(opener)
 
     def __init__(self, search_term):
-        self.search_term = search_term
-        self.count = 0
-        self.start_urls = ["https://www.google.co.in/search?q=" + search_term + "&tbm=isch"]
+        self.search_term = search_term #set search term
+        self.count = 0 #to allow downloading of multiple images
+        self.start_urls = ["https://www.google.co.in/search?q=" + search_term + "&tbm=isch"] #set url to scrape
 
     def parse(self, response):
         print("NOW SEARCHING: " + self.search_term)
-        img_urls = []
-        # img_count = 0
-        # for imagecolumn in response.css(".photos__column"):#response.xpath("//div[@class=\"photos__column\"]"):
-        #     if(not imagecolumn.xpath(".//a[@class=\"js-photo-link photo-item__link\"]")):
-        #         break
-        #     img_url = (imagecolumn.xpath(".//a[@class=\"js-photo-link photo-item__link\"]/img/@data-big-src").extract_first())#.replace(".jpeg", ".jpg").split(".jpg")[0] + ".jpg"
-        #     img_count += 1
-        #     yield {
-        #         "image": img_url
-        #     }
-        #     urllib.request.urlretrieve(img_url, "Images/" + self.wordlist[ImageSpider.count] + " " + str(img_count) + ".jpg")
-        
-        img_urls = response.xpath("//img/@src").extract()
+        img_urls = response.xpath("//img/@src").extract() #extracts url from all images present
         for i in range(0, 1):
             print("static/uploads/" + self.search_term + " " + str(i) + ".jpg")
+            #download image and save it
             urllib.request.urlretrieve(img_urls[i], "static/uploads/" + self.search_term + " " + str(i) + ".jpg")
 
 
-        # if(ImageSpider.count < len(self.wordlist)):
-        #     ImageSpider.count += 1
-        #     yield scrapy.Request(response.urljoin(
-        #         "https://www.google.co.in/search?q=" + self.wordlist[ImageSpider.count] + "&tbm=isch"),
-        #         callback=self.parse, dont_filter=True)
-
 def runSpider(search_term):
+    #runs spider from command line
     os.system("scrapy runspider google_images_scraper.py  -a search_term=" + '"' + str(search_term) + '"')
-    # process = CrawlerProcess({
-    #     'USER_AGENT': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36'
-    # })
-    # process = CrawlerProcess()
-    # process = CrawlerRunner()
-    # process.crawl(ImageSpider, search_term=search_term)
-    # process.start()
-    # reactor.run()
 
-
+#for testing
 if __name__ == "__main__":
     runSpider("jam")
