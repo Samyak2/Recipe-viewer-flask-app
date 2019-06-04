25e919f064f6af9dfdfa0937b6c4709786bf9b9c
Samyak S Sarnayak <samyak201@gmail.com>
2019-06-04 15:28:00 +0530

    Still trying to get heroku working

--- flask_img2txt.py
+++ flask_img2txt.py
@@ -59,8 +59,8 @@ def uploaded_file(filename):
     return send_from_directory(UPLOAD_FOLDER, filename) #send image
 
 #run flask app when script is run directly
-# if __name__ == "__main__":
+if __name__ == "__main__":
     # app.debug = False
     # app.run()
-port = int(os.environ.get("PORT", 5000))
-app.run(debug=True, host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
