97cdd11d597d14cd31631755c556cf216e9910b3
Samyak S Sarnayak <samyak201@gmail.com>
2019-06-05 16:28:47 +0530

    Optimized image to string

--- img2text.py
+++ img2text.py
@@ -1,19 +1,20 @@
 import cv2
-import numpy as np
-import pytesseract
-from PIL import Image
+from numpy import ones, uint8
+from pytesseract import image_to_string
 
 # Path of working folder on Disk
 
 def ocr_core(img_path):
     #Read image with opencv
     img = cv2.imread(img_path)
-    img = cv2.resize(img, None, fx=5, fy=5, interpolation=cv2.INTER_CUBIC)
+    height, width = img.shape[:2]
+    if height<1000 or width<1000:
+        img = cv2.resize(img, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
     # Convert to gray
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # Apply dilation and erosion to remove some noise
-    kernel = np.ones((1, 1), np.uint8)
+    kernel = ones((1, 1), uint8)
     img = cv2.dilate(img, kernel, iterations=1)
     img = cv2.erode(img, kernel, iterations=1)
 
@@ -21,6 +22,6 @@ def ocr_core(img_path):
     cv2.imwrite("outs.png", img)
 
     # Recognize text with tesseract for python
-    result = pytesseract.image_to_string(img)
+    result = image_to_string(img)
 
     return result
